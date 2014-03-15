a974f32fbd3023129a761266b13ebf3ac3e6bbde
Wolfgang Imig <wolfgang.imig@googlemail.com>
2014-03-15 22:39:07 +0100

    Upload function.

---
+++ jsfs-ser/cs/src-ser/com/wilutions/jsfs/BSerializer_1381128722.cs
@@ -0,0 +1,43 @@
+﻿//
+// Serializer for com.wilutions.jsfs.FileSystemServiceC
+// 
+// THIS FILE HAS BEEN GENERATED. DO NOT MODIFY.
+//
+
+using System;
+using System.Collections.Generic;
+using byps;
+
+namespace com.wilutions.jsfs
+{
+	
+	public class BSerializer_1381128722 : BSerializer {
+		
+		public readonly static BSerializer instance = new BSerializer_1381128722();
+		
+		public BSerializer_1381128722()
+			: base(1381128722) {}
+		
+		public BSerializer_1381128722(int typeId)
+			: base(typeId) {}
+		
+		
+		public override void write(Object obj1, BOutput bout1, long version)
+		{
+			FileSystemServiceC obj = (FileSystemServiceC)obj1;			
+			BOutputBin bout = (BOutputBin)bout1;
+			BBufferBin bbuf = bout.bbuf;
+		}
+		
+		public override Object read(Object obj1, BInput bin1, long version)
+		{
+			BInputBin bin = (BInputBin)bin1;
+			FileSystemServiceC obj = (FileSystemServiceC)(obj1 != null ? obj1 : bin.onObjectCreated(new FileSystemServiceC()));
+			
+			BBufferBin bbuf = bin.bbuf;
+			
+			return obj;
+		}
+		
+	}
+} // namespace
---
+++ jsfs-ser/cs/src-ser/com/wilutions/jsfs/FileSystemServiceC.cs
@@ -0,0 +1,49 @@
+﻿//
+// 
+// THIS FILE HAS BEEN GENERATED. DO NOT MODIFY.
+//
+
+using System;
+using System.Collections.Generic;
+using byps;
+
+namespace com.wilutions.jsfs
+{
+	
+	/// <summary>
+	/// Constants used in the interface {@link FileSystemService}.
+	/// </summary>
+	public class FileSystemServiceC : BSerializable
+	{
+	
+		#region Constructors
+		
+		public FileSystemServiceC() {
+		}		
+		
+		public FileSystemServiceC(FileSystemServiceC rhs)
+		{
+		}		
+		
+		#endregion
+		
+		#region Properties
+		
+		#endregion
+		
+		#region Fields
+		
+		/// <summary>
+		/// This symbol stands for a users document folder.
+		/// </summary>
+		/// <remarks>
+		/// It can be used as a placeholder for the documents directory.
+		/// </remarks>
+		public const String FOLDERID_Documents = "{FDD39AD0-238F-46AF-ADB4-6C85480369C7}";
+
+		#endregion
+		
+		
+		public static readonly long serialVersionUID = 1381128722L;		
+	} // end class
+}  // end namespace
